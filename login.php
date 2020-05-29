5a10280cf5bd8cdf0fdec3fd3949d3564e0c4da2
knorrsoupy <souparnarroy@gmail.com>
2020-05-30 02:50:39 +0530

    connection refused problem

--- login.php
+++ login.php
@@ -1,5 +1,10 @@
 <?php
-session_start();  
+session_start(); 
+$host = "ec2-34-232-147-86.compute-1.amazonaws.com";
+$user = "ntvwxnohpujldg";
+$password = "7e7e88712cf85a9006835b3c113eee73628ff35e463ea7c96f4aee8b97def414";
+$dbname = "dbhmbk8ogf0ogk";
+$port = "5432"; 
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
   header("location: welcome.php");
   exit;
@@ -11,7 +16,7 @@ $username_err = $password_err = "";
 $message="";
 try  
  {    
-	$connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);  
+$connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);  
       $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       if(isset($_POST["login"]))  
       {  
