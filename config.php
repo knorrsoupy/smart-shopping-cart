5e6d35d94f6aebd16071dfb8fd4c481d5dfec059
knorrsoupy <souparnarroy@gmail.com>
2020-05-29 21:27:43 +0530

    modified

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'root@');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'project');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
