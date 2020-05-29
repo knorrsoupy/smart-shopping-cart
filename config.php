51f633c598c27b6bb461c49a79301b8ad0074ed9
knorrsoupy <souparnarroy@gmail.com>
2020-05-30 01:01:41 +0530

    config.php added

--- config.php
+++ config.php
@@ -1,11 +1,23 @@
 <?php
-define('DB_SERVER', 'ec2-34-232-147-86.compute-1.amazonaws.com');
-define('DB_USERNAME', 'ntvwxnohpujldg');
-define('DB_PASSWORD', '7e7e88712cf85a9006835b3c113eee73628ff35e463ea7c96f4aee8b97def414');
-define('DB_NAME', 'dbhmbk8ogf0ogk');
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+$host = "ec2-34-232-147-86.compute-1.amazonaws.com";
+$user = "ntvwxnohpujldg";
+$password = "7e7e88712cf85a9006835b3c113eee73628ff35e463ea7c96f4aee8b97def414";
+$dbname = "dbhmbk8ogf0ogk";
+$port = "5432";
+
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-?>
+catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
+  ?>
