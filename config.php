66a8259965a4e5bd8447d9ae24f37d6138e3de94
knorrsoupy <souparnarroy@gmail.com>
2020-05-30 20:15:50 +0530

    backup

--- config.php
+++ config.php
@@ -1,27 +1,22 @@
 <?php
-session_start();
-/* DATABASE CONFIGURATION */
-define('DB_SERVER', "ec2-34-232-147-86.compute-1.amazonaws.com");
-define('DB_USERNAME', "ntvwxnohpujldg");
-define('DB_PASSWORD', "7e7e88712cf85a9006835b3c113eee73628ff35e463ea7c96f4aee8b97def414");
-define('DB_DATABASE', "dbhmbk8ogf0ogk");
+$host = "ec2-34-232-147-86.compute-1.amazonaws.com";
+$user = "ntvwxnohpujldg";
+$password = "7e7e88712cf85a9006835b3c113eee73628ff35e463ea7c96f4aee8b97def414";
+$dbname = "dbhmbk8ogf0ogk";
 $port = "5432";
 
-function getDB()
-{
-$dbhost=DB_SERVER;
-$dbuser=DB_USERNAME;
-$dbpass=DB_PASSWORD;
-$dbname=DB_DATABASE;
-try {
-$dbConnection = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-$dbConnection->exec("set names utf8");
-$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-return $dbConnection;
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
 echo 'Connection failed: ' . $e->getMessage();
 }
-
-}
-?>
+  ?>
