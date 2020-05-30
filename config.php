f831ebca37a0ee845566461604faa09b92066d69
knorrsoupy <souparnarroy@gmail.com>
2020-05-30 21:53:41 +0530

    x

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER','185.27.134.10');
+define('DB_SERVER','http://185.27.134.10/db_structure.php?db=unaux_25898807_project');
 define('DB_USERNAME','unaux_25898807');
 define('DB_PASSWORD', 'qpmw4kn1l');
 define('DB_NAME', ' unaux_25898807_project');
