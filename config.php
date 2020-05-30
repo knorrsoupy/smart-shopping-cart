1d40943c3e174436ee234b08772369913cea9c4a
knorrsoupy <souparnarroy@gmail.com>
2020-05-30 21:22:08 +0530

    added port

--- config.php
+++ config.php
@@ -3,7 +3,8 @@ define('DB_SERVER', 'fdb23.awardspace.net');
 define('DB_USERNAME', '3370068_qrcode');
 define('DB_PASSWORD', 'qwerty1234@');
 define('DB_NAME', '3370068_qrcode');
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+define('DB_PORT','3306');
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME,DB_PORT);
 
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
