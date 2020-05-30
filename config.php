0b09cba2862c8c6e4ddb426df7b3d48ea8c81ace
knorrsoupy <souparnarroy@gmail.com>
2020-05-30 22:04:01 +0530

    localhost attempt

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','sql304.unaux.com');
-define('DB_USERNAME','unaux_25898807');
-define('DB_PASSWORD', 'qpmw4kn1l');
-define('DB_NAME', 'unaux_25898807_project');
+define('DB_SERVER','127.0.0.1');
+define('DB_USERNAME','soup');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'project');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
