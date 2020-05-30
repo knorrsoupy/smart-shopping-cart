b13efa6cee8b4ee52ce57f51bcf157f4ebfe0a08
knorrsoupy <souparnarroy@gmail.com>
2020-05-30 23:31:16 +0530

    ookay

--- config.php
+++ config.php
@@ -1,12 +1,22 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USERNAME','soup');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'project');
+$host = "eec2-34-200-72-77.compute-1.amazonaws.com";
+$user = "xlirvlyttavfzt";
+$password = "9e0f3404439664f156b9c7e87a8a1b7ed1e380511b0ab29de72172322166bbbb";
+$dbname = "d8ekmps2cf9i0o";
+$port = "5432";
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
 
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-?>
+catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
+  ?>
